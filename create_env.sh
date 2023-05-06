e878366ac3179097ec589efca3389ba7ac67dc24
Eric Langezaal <eric.langezaal@gmail.com>
2023-05-06 17:21:42 +0200

    fixed all environment issues!

---
+++ create_env.sh
@@ -0,0 +1,12 @@
+if ! command -v conda &> /dev/null
+then
+    module load 2021
+    module load Anaconda3/2021.05
+fi
+conda create -n dl2 python=3.10.11 -y
+source activate dl2
+conda install cudatoolkit -y
+pip install ipykernel accelerate==0.18.0 bitsandbytes==0.38.1
+pip install git+https://github.com/salesforce/LAVIS.git
+cd ~/.conda/envs/dl2/lib/python3.10/site-packages/bitsandbytes
+cp libbitsandbytes_cuda117.so libbitsandbytes_cpu.so
