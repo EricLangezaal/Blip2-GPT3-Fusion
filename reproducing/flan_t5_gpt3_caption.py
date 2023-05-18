ed09b73a2e6405d1ca7912382f7bf8d2a6b534c8
Daniel-van-Dijk <danielgerald@live.nl>
2023-05-18 15:29:11 +0200

    add caption generation to flan

--- reproducing/flan_t5_gpt3_caption.py
+++ reproducing/flan_t5_gpt3_caption.py
@@ -15,6 +15,11 @@ from lavis.common.registry import registry
 from lavis.models.blip2_models.blip2 import Blip2Base, disabled_train
 from lavis.models.blip2_models.modeling_t5 import T5Config, T5ForConditionalGeneration
 
+import openai
+from openai.error import RateLimitError
+
+from gpt_utils import *
+import time
 
 @registry.register_model("blip2_t5_gpt3_caption")
 class FlanGPTCaption(Blip2Base):
@@ -203,6 +208,7 @@ class FlanGPTCaption(Blip2Base):
 
         if "prompt" in samples.keys():
             prompt = samples["prompt"]
+            print('prompt = ', prompt)
         else:
             prompt = self.prompt
 
@@ -254,58 +260,75 @@ class FlanGPTCaption(Blip2Base):
         length_penalty=-1,
         **kwargs
     ):
-        image = samples["image"]
-        with self.maybe_autocast():
-            image_embeds = self.ln_vision(self.visual_encoder(image))
-        image_embeds = image_embeds.float()
-        image_atts = torch.ones(image_embeds.size()[:-1], dtype=torch.long).to(
-            image.device
-        )
+        try: 
+            #################### ADDED PART  ########################################
+            # GENERATION OF OBJECT DESCRIPTION
+            openai_api_key = "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"
+            openai.api_key = openai_api_key
+            paper_prompt = "a photo of"
+            extra_prompt1 = "The action happening in this picture are"
+            #extra_prompt2 = "In this picture, I see"
+            extra_prompt3 = "The location of this picture is"
+            extra_prompt4 = "The weather looks"
+            
+            nouns = questionlemmatize(samples['text_input'])
+            #print(samples['text_input'])
+            #print(nouns)
+            prompts = [paper_prompt, extra_prompt1, extra_prompt3, extra_prompt4]
+            #prompts = [[extra_prompt1]] * len(samples['text_input'])
+            for prompt in prompts: 
+                samples['prompt'] = prompt
+                #gpt_questions = gpt_generate_questions(text_input)
+                #print(gpt_questions)
+                listed_answers = []
+                #for batch in list(zip(*prompts)):               
 
-        query_tokens = self.query_tokens.expand(image_embeds.shape[0], -1, -1)
-        query_output = self.Qformer.bert(
-            query_embeds=query_tokens,
-            encoder_hidden_states=image_embeds,
-            encoder_attention_mask=image_atts,
-            return_dict=True,
-        )
+                answer_to_gpt_embed = self.generate(
+                        samples=samples,
+                        use_nucleus_sampling=True,
+                        num_beams=5,
+                        max_length=30,
+                        min_length=1,
+                        #top_p=0.9,
+                        repetition_penalty=1.5,
+                        length_penalty=2.0,
+                        num_captions=1, 
+                        )
+                print(answer_to_gpt_embed)
+            """
+            gpt_summarised_batch = []
+            for questions, answers, org_question, org_answer in zip(prompts, listed_answers, text_input, output_text):
+                gpt_summarised_batch.append(summarized_gpt(questions, answers, org_question, org_answer))
 
-        inputs_t5 = self.t5_proj(query_output.last_hidden_state)
-        atts_t5 = torch.ones(inputs_t5.size()[:-1], dtype=torch.long).to(image.device)
+            if self._apply_lemmatizer:
+                gpt_summarised_batch = self._lemmatize(gpt_summarised_batch)
+            
 
-        if isinstance(samples["text_input"], str):
-            samples["text_input"] = [samples["text_input"]]
-        if prompt:
-            text_input = [prompt.format(question) for question in samples["text_input"]]
-        else:
-            text_input = samples["text_input"]
+            print('new batch')
+            for gpt_summarized, questions, answers, org_question, org_answer in zip(gpt_summarised_batch, gpt_questions, listed_answers, text_input, output_text):
+                print("Original question: ", org_question)
+                print("Original answer: ", org_answer)
+                print("GPT generated questions:", questions)
+                print("THe answers to those GPT questions:", answers)
+                print("FINAL answer: ", gpt_summarized, "\n")        
+            ################ ADDED PART ######################
 
-        input_tokens = self.t5_tokenizer(
-            text_input, padding="longest", return_tensors="pt"
-        ).to(image.device)
-
-        encoder_atts = torch.cat([atts_t5, input_tokens.attention_mask], dim=1)
-
-        inputs_embeds = self.t5_model.encoder.embed_tokens(input_tokens.input_ids)
-        inputs_embeds = torch.cat([inputs_t5, inputs_embeds], dim=1)
-
-        outputs = self.t5_model.generate(
-            inputs_embeds=inputs_embeds,
-            attention_mask=encoder_atts,
-            do_sample=False,
-            num_beams=num_beams,
-            max_new_tokens=max_len,
-            min_length=min_len,
-            length_penalty=length_penalty,
-        )
-        output_text = self.t5_tokenizer.batch_decode(
-            outputs, skip_special_tokens=True
-        )
-
-        if self._apply_lemmatizer:
-            output_text = self._lemmatize(output_text)
-
-        return output_text
+            return gpt_summarised_batch
+            """
+        except RateLimitError:
+            time.sleep(5)
+            return self.predict_answers(
+                samples,
+                num_beams,
+                inference_method,
+                max_len,
+                min_len,
+                num_ans_candidates,
+                answer_list,
+                prompt,
+                length_penalty,
+                **kwargs
+            )
 
     def _lemmatize(self, answers):
         def apply(answer):
@@ -318,10 +341,12 @@ class FlanGPTCaption(Blip2Base):
                 else:
                     words.append(token.text)
             answer = " ".join(words)
-
+            
             return answer
 
         return [apply(answer) for answer in answers]
+    
+    
 
     @property
     def lemmatizer(self):
@@ -377,3 +402,21 @@ class FlanGPTCaption(Blip2Base):
         model.load_checkpoint_from_config(cfg)
 
         return model
+
+def questionlemmatize(questions):
+    import spacy
+    lemmatizer = spacy.load("en_core_web_sm")
+    def apply(questions):
+        doc = lemmatizer(questions)
+
+        nouns = []
+        for token in doc:
+            if token.pos_ in ["NOUN"]:
+                nouns.append(token.text)
+            #else:
+            #    words.append(token.text)
+        #answer = " ".join(words)
+
+        return nouns
+
+    return [apply(question) for question in questions]
